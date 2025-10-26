7953e73bbbfab869863afafecc6ee5d2bc7fc765
Mikhail Mozhaev <programmer10110@gmail.com>
2025-10-26 21:44:58 +0200

    tavily support to extract web contract

--- mcp_server.py
+++ mcp_server.py
@@ -20,6 +20,8 @@ import re
 import httpx
 from mcp.server.fastmcp import FastMCP
 
+from tavily import TavilyClient
+
 
 DEFAULT_BASE_URL = "http://whoogle-search:5000"
 SEARCH_PATH = "/search"
@@ -29,62 +31,6 @@ MAX_LINKS_IN_RESPONSE = 3
 HEADERS = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.6668.100 Safari/537.36"
         }
-
-class HelpFunctions:
-    def __init__(self):
-        pass
-
-    def get_base_url(self, url):
-        parsed_url = urlparse(url)
-        base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-        return base_url
-
-    def generate_excerpt(self, content, max_length=200):
-        return content[:max_length] + "..." if len(content) > max_length else content
-
-    def format_text(self, original_text):
-        soup = BeautifulSoup(original_text, "html.parser")
-        formatted_text = soup.get_text(separator=" ", strip=True)
-        formatted_text = unicodedata.normalize("NFKC", formatted_text)
-        formatted_text = re.sub(r"\s+", " ", formatted_text)
-        formatted_text = formatted_text.strip()
-        formatted_text = self.remove_emojis(formatted_text)
-        return formatted_text
-
-    def remove_emojis(self, text):
-        return "".join(c for c in text if not unicodedata.category(c).startswith("So"))
-
-    def process_search_result(self, result):
-        title_site = self.remove_emojis(result["title"])
-        url_site = result["url"]
-        snippet = result.get("content", "")
-
-        try:
-            response_site = requests.get(url_site, timeout=20)
-            response_site.raise_for_status()
-            html_content = response_site.text
-
-            soup = BeautifulSoup(html_content, "html.parser")
-            content_site = self.format_text(soup.get_text(separator=" ", strip=True))
-
-            truncated_content = self.truncate_to_n_words(
-                content_site, PAGE_CONTENT_WORDS_LIMIT
-            )
-
-            return {
-                "title": title_site,
-                "url": url_site,
-                "content": truncated_content,
-                "snippet": self.remove_emojis(snippet),
-            }
-
-        except requests.exceptions.RequestException as e:
-            return None
-
-    def truncate_to_n_words(self, text, token_limit):
-        tokens = text.split()
-        truncated_tokens = tokens[:token_limit]
-        return " ".join(truncated_tokens)
 
 mcp = FastMCP("whoogle-web-search")
 
@@ -107,50 +53,16 @@ async def get_website(url: str) -> str:
         """
         Web scrape the website provided and get the content of it.
         :params url: The URL of the website.
-        :return: The content of the website in json format.
+        :return: The content of the website in markdown format.
         """
-        functions = HelpFunctions()
+        
+        tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+        response = tavily_client.extract(url, format="markdown")
 
-        results_json = []
+        if len(response["failed_results"]) > 0:
+            return response["failed_results"][0]["error"]
 
-        try:
-            response_site = requests.get(url, headers=HEADERS, timeout=120)
-            response_site.raise_for_status()
-            html_content = response_site.text
-
-            soup = BeautifulSoup(html_content, "html.parser")
-
-            page_title = soup.title.string if soup.title else "No title found"
-            page_title = unicodedata.normalize("NFKC", page_title.strip())
-            page_title = functions.remove_emojis(page_title)
-            title_site = page_title
-            url_site = url
-            content_site = functions.format_text(
-                soup.get_text(separator=" ", strip=True)
-            )
-
-            truncated_content = functions.truncate_to_n_words(
-                content_site, PAGE_CONTENT_WORDS_LIMIT
-            )
-
-            result_site = {
-                "title": title_site,
-                "url": url_site,
-                "content": truncated_content,
-                "excerpt": functions.generate_excerpt(content_site),
-            }
-
-            results_json.append(result_site)
-
-        except requests.exceptions.RequestException as e:
-            results_json.append(
-                {
-                    "url": url,
-                    "content": f"Failed to retrieve the page. Error: {str(e)}",
-                }
-            )
-
-        return json.dumps(results_json, ensure_ascii=False)
+        return response["results"][0]["raw_content"]
 
 
 @mcp.tool()
@@ -166,7 +78,7 @@ async def web_search(query: str) -> str:
     payload = await _perform_search(query.strip())
     n = min(len(payload["results"]), MAX_LINKS_IN_RESPONSE)
     for i in range(0, n):
-        content = json.loads(await get_website(payload["results"][i]["href"]))[0]["content"]
+        content = await get_website(payload["results"][i]["href"])
         payload["results"][i]["text"] = content
         del payload["results"][i]["content"]
     payload["results"] = payload["results"][0:n]
